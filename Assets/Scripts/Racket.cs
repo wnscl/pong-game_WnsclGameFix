8d415b6f27b616fa28d0880825c571b773529595
Wnscl <Wnscl1175@gmail.com>
2026-09-20 15:09:43 +0900

    Feature_ItemEffectText
    아이템을 받으면 어떤 아이템 효과가 적용중인지 플레이어 정보 ui위에 텍스트를 만들어 플레이어가 알 수 있도록 하였습니다.
    또한 아이템 효과는 반드시 하나의 효과만 적용받도록 수정하였습니다.

--- Assets/Scripts/Racket.cs
+++ Assets/Scripts/Racket.cs
@@ -5,15 +5,27 @@ using UnityEngine.UI;
 
 public abstract class Racket : MonoBehaviour
 {
+    [Header("Ref")]
     public Rigidbody2D rb;
+    public Text scoreText;
+    public Text itemText;
+
+    [Header("Status")]
     public float moveSpeed;
     public float defaultMoveSpeed;
     public float itemMoveSpeed;
     public string AxesName;
-    public bool isScoreIncreased = false;
     public int Score { get; private set; }
-    public Text scoreText;
+    public bool isScoreIncreased = false;
+    [SerializeField] private ItemType myItemType;
 
+    private Coroutine itemTextCoroutine;
+    private void Start()
+    {
+        InitializeItemEffect();
+        itemText.text = $"None : 0.0 / 5.0";
+        itemText.color = Color.white;
+    }
     void FixedUpdate()
     {
         Movement();
@@ -46,51 +58,70 @@ public abstract class Racket : MonoBehaviour
 
     public void ApplyItemEffectToRacket(ItemType itemType)
     {
+        myItemType = itemType;
+        CancelInvoke(nameof(InitializeItemEffect));
+        InitializeItemEffect();
+        if (itemTextCoroutine != null)
+        {
+            StopCoroutine(itemTextCoroutine);
+            itemTextCoroutine = null;
+        }
+        
         switch (itemType)
         {
             case ItemType.SpeedItem:
-                CancelInvoke(nameof(SpeedDown));
                 SpeedUp();
-                Invoke("SpeedDown", 5.0f);
+                itemText.color = Color.red;
                 break;
             case ItemType.ScoreItem:
-                CancelInvoke(nameof(ScoreDown));
                 ScoreUp();
-                Invoke("ScoreDown", 5.0f);
+                itemText.color = Color.green;
                 break;
             case ItemType.SizeItem:
-                CancelInvoke(nameof(SizeDown));
                 SizeUp();
-                Invoke("SizeDown", 5.0f);
+                itemText.color= Color.yellow;
                 break;
             default:
                 break;
         }
+        itemTextCoroutine = StartCoroutine(itemDurationTextControl());
+        Invoke("InitializeItemEffect", 5f);
     }
-
     private void SpeedUp()
     {
         moveSpeed = itemMoveSpeed;
-    }
-    private void SpeedDown()
-    {
-        moveSpeed = defaultMoveSpeed;
     }
     private void ScoreUp()
     {
         isScoreIncreased = true;
     }
-    private void ScoreDown()
-    {
-        isScoreIncreased = false;
-    }
     private void SizeUp()
     {
         this.transform.localScale = new Vector3(0.47f, 4f, 1);
     }
-    private void SizeDown()
+    private void InitializeItemEffect()
     {
+        moveSpeed = defaultMoveSpeed;
+        isScoreIncreased = false;
         this.transform.localScale = new Vector3(0.47f, 2.5f, 1);
+    }
+    private IEnumerator itemDurationTextControl()
+    {
+        yield return null;
+        int itemDuration = 5;
+        while (itemDuration >= 0)
+        {
+            if (itemDuration == 0)
+            {
+                itemText.text = $"None : 0.0 / 5.0";
+                itemText.color = Color.white;
+            }
+            else
+                itemText.text = $"{myItemType.ToString()} : {itemDuration}/5.0";
+
+            itemDuration--;
+            yield return new WaitForSeconds(1f);
+        }
     }
 
 }
